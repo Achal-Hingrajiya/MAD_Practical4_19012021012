accde91a0d27aee13409fbd7bba5df87eff977e7
Achal Hingrajiya <achalhingrajiya19@gnu.ac.in>
2021-08-29 15:58:32 +0530

    Completed UI and Backend For Opening Call log and Contacts

--- app/src/main/java/com/example/practical4_19012021012/MainActivity.kt
+++ app/src/main/java/com/example/practical4_19012021012/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.practical4_19012021012
 import android.content.Intent
 import android.content.Intent.ACTION_DIAL
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -17,26 +17,44 @@ class MainActivity : AppCompatActivity() {
         val callingBtn = findViewById<Button>(R.id.btn_call)
         val viewPageBtn = findViewById<Button>(R.id.btn_url)
         val textMobileUrlInput = findViewById<TextInputEditText>(R.id.txtInput_mobile_url)
+        val pickContactButton = findViewById<Button>(R.id.btn_pick_contact)
+        val viewCallLogButton = findViewById<Button>(R.id.btn_view_call_log)
 
-        //com.google.android.material.textfield.TextInputLayout
-        //com.google.android.material.textfield.TextInputEditText
 
-        callingBtn.setOnClickListener(View.OnClickListener {
-            val callIntent = Intent(Intent.ACTION_DIAL)
-            callIntent.setData(Uri.parse("tel:" + textMobileUrlInput.text)).apply {
+
+        callingBtn.setOnClickListener {
+            Intent(ACTION_DIAL, Uri.parse("tel:" + textMobileUrlInput.text)).apply {
                 startActivity(this)
             }
-        })
+        }
 
-        viewPageBtn.setOnClickListener(View.OnClickListener {
-            val viewPageIntent = Intent(Intent.ACTION_VIEW)
+        viewPageBtn.setOnClickListener {
 
-            viewPageIntent.setData(Uri.parse("https://" + textMobileUrlInput.text)).apply {
+//            val viewPageIntent = Intent(Intent.ACTION_VIEW)
+
+//            viewPageIntent.setData(Uri.parse("https://" + textMobileUrlInput.text)).apply {
+//                startActivity(this)
+//            }
+
+            Intent(Intent.ACTION_VIEW, Uri.parse("https://" + textMobileUrlInput.text)).apply {
                 startActivity(this)
             }
-        })
+        }
 
 
+
+
+        pickContactButton.setOnClickListener {
+            val pickContactIntent = Intent(Intent.ACTION_PICK)
+
+            pickContactIntent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+            startActivity(pickContactIntent)
+        }
+
+        viewCallLogButton.setOnClickListener {
+            Intent(Intent.ACTION_VIEW, Uri.parse("content://call_log/calls")).apply { startActivity(this) }
+
+        }
     }
 
 
